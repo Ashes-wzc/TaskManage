a30fd719599ae6bd98f1b5a0f94ae7aeaa7c23d9
mitn-leaf <vvmint233@gmail.com>
2021-08-24 23:54:43 +0800

    fix locale

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-// import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import 'element-plus/lib/theme-chalk/index.css'
-import router from './router'
-import App from './App.vue'
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import locale from "element-plus/es/locale/lang/zh-cn";
+import "dayjs/locale/zh-cn";
+import "element-plus/lib/theme-chalk/index.css";
+import router from "./router";
+import App from "./App.vue";
 
-const app = createApp(App)
-app.use(router)
-app.use(ElementPlus)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(ElementPlus, { locale });
+app.mount("#app");
