4e3b3a245be4e05b33cee11501365107314a2ee6
Asheswzc <asheswzc@gmail.com>
2022-03-03 18:37:10 +0800

    new md

--- src/components/views/Office/Template.vue
+++ src/components/views/Office/Template.vue
@@ -10,24 +10,33 @@
           </el-dropdown-menu>
         </template>
       </el-dropdown>
-      <el-button size="small">添加模板</el-button>
+      <el-button size="small" style="margin-left:10px;">
+        <el-icon><Plus /></el-icon>添加模板
+      </el-button>
     </div>
     <div class="template_mainbody">
       <div class="template_display">
         <!-- 计划区域 -->
         <div class="scheme_area">
-          <div class="scheme_title"></div>
+          <div class="scheme_header">
+            <el-input v-model="schemeTitle" placeholder="输入标题"/>
+            <span style="font-size:20px;margin-left:10px;">2</span>
+          </div>
           <!-- 任务区域 -->
           <div class="task_area">
             <div class="task_card"></div>
-            <div>
-              <el-button>添加新任务</el-button>
+            <div class="add_sth_btn">
+              <el-button type="text">
+                <el-icon size="20"><Plus /></el-icon>
+              </el-button>
             </div>
           </div>
         </div>
         <!-- 计划添加框 -->
-        <div class="add_scheme">
-          <el-button type="text">添加新计划</el-button>
+        <div class="add_sth_btn">
+          <el-button type="text">
+            <el-icon size="20"><Plus /></el-icon>
+          </el-button>
         </div>
       </div>
       <div class="template_operation">
@@ -41,10 +50,15 @@
 <script>
 import { onMounted, ref } from 'vue'
 import { getAllTemplateAPI } from '@/utils/api'
+import { Plus } from '@element-plus/icons'
 export default {
   name: 'Template',
+  components: {
+    Plus
+  },
   setup() {
     const showingTemplateName = ref('请选择模板')
+    const schemeTitle = ref('计划标题')
     const templateList = ref([]) // 全部模板的列表
     const getAllTemplate = () => {
       getAllTemplateAPI()
@@ -63,6 +77,7 @@ export default {
       getAllTemplate()
     })
     return {
+      schemeTitle,
       showingTemplateName,
       templateList,
       templateSelected
@@ -84,12 +99,32 @@ export default {
 .template_mainbody {
   height: calc(100% - 50px);
 }
+.scheme_area {
+  width: 150px;
+  margin-right: 10px;
+}
+.scheme_header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
 .template_display {
+  display: flex;
+  flex-direction: row;
   height: calc(100% - 40px);
 }
 .template_operation {
   display: flex;
   justify-content: right;
   height: 40px;
+}
+.add_sth_btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 148px;
+  height: 40px;
+  border: 1px solid #eeeeee;
+  border-radius: 10px;
 }
 </style>
